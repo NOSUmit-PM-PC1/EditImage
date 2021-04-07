b358decd3ac2c6ffb4d2bd14ab07ecb766e15e69
Makarenkomd <mahadrum@yandex.ru>
2021-04-07 16:08:54 +0300

    Add files via upload

---
+++ EditImage/EditImage/FormEditImage.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace EditImage
+{
+    partial class FormEditImage
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonEdit = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonEdit
+            // 
+            this.buttonEdit.Location = new System.Drawing.Point(234, 467);
+            this.buttonEdit.Name = "buttonEdit";
+            this.buttonEdit.Size = new System.Drawing.Size(212, 23);
+            this.buttonEdit.TabIndex = 0;
+            this.buttonEdit.Text = "Редактировать";
+            this.buttonEdit.UseVisualStyleBackColor = true;
+            this.buttonEdit.Click += new System.EventHandler(this.buttonEdit_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(625, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(519, 392);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // FormEditImage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(1249, 518);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.buttonEdit);
+            this.Name = "FormEditImage";
+            this.Text = "Редактор изображений";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.FormEditImage_MouseDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonEdit;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ EditImage/EditImage/FormEditImage.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EditImage
+{
+    public partial class FormEditImage : Form
+    {
+        //Graphics g;
+        Bitmap myBitmap;
+        public FormEditImage()
+        {
+            InitializeComponent();
+            //g = this.CreateGraphics();
+            myBitmap = new Bitmap("26.jpg");
+            this.BackgroundImage = myBitmap;
+        }
+
+        private void buttonEdit_Click(object sender, EventArgs e)
+        {
+
+            for (int x = 0; x < myBitmap.Size.Width; x++)
+                for (int y = 0; y < myBitmap.Size.Height; y++)
+                {
+                    int colorR = myBitmap.GetPixel(x, y).R;
+                    int colorG = myBitmap.GetPixel(x, y).G;
+                    int colorB = myBitmap.GetPixel(x, y).B;
+                    //43.92 % красного, 25.88 % зеленого и 7.84 %
+                    int gray = (int)(colorR * 0.4392 + colorG * 0.2588 + colorB * 0.784);
+                    //gray = Math.Min(255, gray);
+                    Color newColor = Color.FromArgb(5, (int)(colorR * 0.4392), (int)(colorG * 0.2588), (int)(colorB * 0.784));
+                    myBitmap.SetPixel(x, y, newColor);
+                }
+            pictureBox1.Size = myBitmap.Size;
+            pictureBox1.Image = myBitmap;
+            MessageBox.Show("Готово!");
+        }
+
+        private void FormEditImage_MouseDown(object sender, MouseEventArgs e)
+        {
+            Color pixelColor = myBitmap.GetPixel(e.X, e.Y);
+            MessageBox.Show(pixelColor.ToString());
+        }
+    }
+}
